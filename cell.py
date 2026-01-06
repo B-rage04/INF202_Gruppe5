bf6eaee6ee3c9e7ef7cdd227b64067a4f98ad747
OskarNerheim <157756236+OskarNerheim@users.noreply.github.com>
2026-01-06 14:12:21 +0100

    adds area for triangle cell

--- cell.py
+++ cell.py
@@ -1,9 +1,11 @@
+from mesh import Mesh
+
 class Cell:
-    def __init__(self, msh):
+    def __init__(self, msh, n):
         self.oc = 0 #oil-count
         self.ngb = [] #neigbours
-        self.id = "Oskar"
-        self.cords = msh.cells[n].data
+        self.id = str(n)
+        self.cords = msh.cells[3].data[n]
 
     def neigbor_calculate(self, cell):
         if self.cords[0] in cell.cords and self.cords[1] in cell.cords:
@@ -19,3 +21,8 @@ class Cell:
         else:
             for c in msh.cells:
                 self.neigbor_calculate(self,c)
+
+    def area(self):
+        return 0.5 * abs((self.cords[0][0] - self.cords[2][0])(self.cords[1][1] - self.cords[0][1]) - (self.cords[0][0] - self.cords[1][0])(self.cords[2][1] - self.cords[0][1]))
+    
+jeff = Cell(Mesh)
