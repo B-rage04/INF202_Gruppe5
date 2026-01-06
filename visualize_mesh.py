c0d266dc8ec8807a36bf561f3f34f90c98f1fe22
lalicon05 <lasse.l535@gmail.com>
2026-01-06 12:35:06 +0100

    reverted to matplotlib for simplicity

--- visualize_mesh.py
+++ visualize_mesh.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 import meshio
 import numpy as np
 
-import pyvista as pv
 
 mesh = meshio.read("bay.msh")
 
-pl = pv.Plotter()
+points = mesh.points
+triangles = mesh.cells_dict["triangle"]
 
-pl.add_mesh(mesh, color="blue", show_edges=True, cmap="virdis")
-
-pl.show()
+plt.triplot(points[:, 0], points[:, 1], triangles)
+plt.show()
