f06943c2701220b9e9dc10d7dc20475f44c9af98
lalicon05 <lasse.l535@gmail.com>
2026-01-06 13:04:49 +0100

    added visualize to mesh class

--- mesh.py
+++ mesh.py
@@ -1,6 +1,18 @@
 class Mesh:
-    def __init__(self):
+    def __init__(self, file):
         import meshio
-        self.msh = meshio.read("bay.msh")
+        self.msh = meshio.read(file)
         self.cells = self.msh.cells[8:12] #Do not include the vortex cells
         self.points = self.msh.points
+    
+    def visialize(self):
+        import matplotlib.pyplot as plt
+
+        points = self.msh.points
+        triangles = self.msh.cells_dict["triangle"]
+
+        plt.triplot(points[:, 0], points[:, 1], triangles)
+        plt.show()
+
+mashimash = Mesh("bay.msh")
+mashimash.visialize()
