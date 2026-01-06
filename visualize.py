b320ac61bdfd56e0cec5d995cfa973369cdd28ba
lalicon05 <lasse.l535@gmail.com>
2026-01-06 15:24:47 +0100

    made visualizer

---
+++ visualize.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+from mesh import Mesh
+import numpy as np
+
+class Visualizer:
+    def __init__(self, mesh):
+        self.mesh = mesh
+    
+    def plotting(self, u, filename = None):
+
+        custom_cmap = mcolors.LinearSegmentedColormap.from_list("blarm", ["cadetblue", "black"])
+        plt.figure()
+        plt.tripcolor(
+                    self.mesh.points[:,0],
+                    self.mesh.points[:,1],
+                    self.mesh.triangles,
+                    u,
+                    shading="flat",
+                    cmap=custom_cmap
+        )
+
+        points = self.mesh.points
+        triangles = self.mesh.triangles
+
+        plt.triplot(points[:, 0], points[:, 1], triangles, color="black")
+        plt.colorbar(label="Oil concentration")
+        plt.show()
+
+        if filename:
+            pass
+        else:
+            plt.show()
+
+vs = Visualizer(Mesh("bay.msh"))
+n_cells = len(vs.mesh.triangles)
+vs.plotting(u = np.random.rand(n_cells))
