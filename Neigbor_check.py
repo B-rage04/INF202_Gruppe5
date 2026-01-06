4e177349e0f57764edc3591b006f4076dd9614b6
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-06 11:02:15 +0100

    finne naboer

--- Neigbor_check.py
+++ Neigbor_check.py
@@ -1,12 +1,9 @@
 def neigbor_check(cell1, cell2):
-    """
-    Checks if two cells are neigbors
-    """
-    if cell1[0] == cell2[0] and cell1[1] == cell2[1]:
+    if cell1[0] in cell2 and cell1[1] in cell2:
         return True
-    elif cell1[1] == cell2[1] and cell1[2] == cell2[2]:
+    elif cell1[1] in cell2 and cell1[2] in cell2:
         return True
-    elif cell1[0] == cell2[0] and cell1[2] == cell2[2]:
+    elif cell1[0] in cell2 and cell1[2] in cell2:
         return True
     else:
         return False
