bc0d13ee12cce995ef9d2c24729f9eb15e735a19
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-06 13:19:31 +0100

    as

--- cell.py
+++ cell.py
@@ -1,9 +1,9 @@
 class Cell:
-    def __init__(self, i, cell):
+    def __init__(self, msh):
         self.oc = 0 #oil-count
         self.ngb = [] #neigbours
-        self.id = i
-        self.cords = cell[n].data
+        self.id = "Oskar"
+        self.cords = msh.cells[n].data
 
     def neigbor_calculate(self, cell):
         if self.cords[0] in cell.cords and self.cords[1] in cell.cords:
@@ -13,10 +13,9 @@ class Cell:
         elif self.cords[0] in cell.cords and self.cords[2] in cell.cords:
             self.ngb.append(cell)
 
-    def neighbor_chech(self):
+    def neighbor_chech(self, msh):
         if len(self.cords) == len(self.ngb):
             return self.ngb
         else:
-            for c in cells
+            for c in msh.cells:
                 self.neigbor_calculate(self,c)
-        
---
+++ main.py
@@ -0,0 +1,6 @@
+from mesh import Mesh
+from cell import Cell
+msh = Mesh()
+
+
+for n in range(len())
