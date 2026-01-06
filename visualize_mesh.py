732df09e2ca2a12523194af9eecd4fbcd378cb69
lalicon05 <lasse.l535@gmail.com>
2026-01-06 11:41:16 +0100

    Update requirements and use a 3d library to visualize the mesh

--- visualize_mesh.py
+++ visualize_mesh.py
@@ -1,2 +1,13 @@
-import matplotlib
+import matplotlib.pyplot as plt
 import meshio
+import numpy as np
+
+import pyvista as pv
+
+mesh = meshio.read("bay.msh")
+
+pl = pv.Plotter()
+
+pl.add_mesh(mesh, color="blue", show_edges=True, cmap="virdis")
+
+pl.show()
