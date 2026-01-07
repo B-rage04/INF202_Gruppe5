a128a0dfc5380b8f7abda3ade6feeb110d78d950
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-07 20:48:35 +0100

    Refactor simulation logic and add Simulation class for mesh processing

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from src.Cells.cell import Cell
+from src.simulation import Simulation
 from src.mesh import Mesh
-from src.visualize import Visualizer
 
-msh = Mesh("bay.msh")
-
-cells = []
-for i in range(len(msh.triangles)):
-    cells.append(Cell(msh, i))
-
-
-oil_val = []
-
-for cel in cells:
-    oil_val.append(cel.oil)
-
-vs = Visualizer(msh)
-vs.plotting(oil_val)
+sim = Simulation(Mesh("bay.msh"))
+sim.run_sim()
