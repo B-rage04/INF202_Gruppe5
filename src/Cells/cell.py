8f2ab102e6d56920c0f67e515528933aeca8d735
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-07 13:09:38 +0100

    a

--- src/Cells/cell.py
+++ src/Cells/cell.py
@@ -11,8 +11,6 @@ class Cell:
         self.center_point = Mesh.triangle_mid(self.cords)
 
     def neigbor_calculate(self, cell):
-<<<<<<< HEAD
-        for i in range(len(self.cords)):
             for j in range(len(cell.cords)):
                 if self.cords[i] in cell.cords and self.cords[j] in cell.cords:
                     if cell in self.ngb:
@@ -20,15 +18,6 @@ class Cell:
                     else:
                         self.ngb.append(cell)
         
-=======
-        if self.cords[0] in cell.cords and self.cords[1] in cell.cords:
-            self.ngb.append(cell)
-        elif self.cords[1] in cell.cords and self.cords[2] in cell.cords:
-            self.ngb.append(cell)
-        elif self.cords[0] in cell.cords and self.cords[2] in cell.cords:
-            self.ngb.append(cell)
-
->>>>>>> Dev
     def neighbor_check(self, msh):
         if len(self.cords) == len(self.ngb):
             return self.ngb
