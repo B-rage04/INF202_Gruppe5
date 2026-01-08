faf3fb7b0ea4bb762ca209f2c8bb7dfd3a1fafb6
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-08 10:12:08 +0100

    formatting

--- src/simulation.py
+++ src/simulation.py
@@ -2,12 +2,13 @@ from src.Cells.cell import Cell
 from src.mesh import Mesh
 from src.visualize import Visualizer
 
+
 class Simulation:
     def __init__(self, msh):
         self.cells = [Cell(msh, i) for i in range(len(msh.triangles))]
         self.oil_vals = [cell.oil for cell in self.cells]
         self.vs = Visualizer(msh)
         self.time = 0
+
     def run_sim(self):
         self.vs.plotting(self.oil_vals)
-        
