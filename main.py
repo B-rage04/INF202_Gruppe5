77669f22de47b45fb3336e42c14d7e0eaf0bafc2
lalicon05 <lasse.l535@gmail.com>
2026-01-09 10:45:31 +0100

    raa

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from src.mesh import Mesh
 from src.simulation import Simulation
 
 sim = Simulation(Mesh("bay.msh"))
-# sim.run_sim()
+sim.run_sim()
 print(sim.cells[-5].id)
 print(sim.cells[-5].ngb)
-print(sim.cells[3437].id)
-print(sim.cells[3437].id)
+print(sim.cells[3615].id)
+print(sim.cells[3615].ngb)
