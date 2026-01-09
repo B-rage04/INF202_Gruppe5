f4fd07ead6497677ea3def9a343200498844df15
VikingInOrbit <104018760+VikingInOrbit@users.noreply.github.com>
2026-01-09 11:14:19 +0100

    time chek

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import time
-
+ti_bib = time.time()
 from src.mesh import Mesh
 from src.simulation import Simulation
 
-ti = time.time()
+ti_ngh = time.time()
+
 sim = Simulation(Mesh("bay.msh"))
-sim.run_sim()
+#sim.run_sim()
 print(sim.cells[-5].id)
 print(sim.cells[-5].ngb)
 print(sim.cells[3615].id)
 print(sim.cells[3615].ngb)
+
+tf = time.time()
+print("Time elapsed bib:", tf - ti_bib)
+print("Time elapsed ngh:", tf - ti_ngh)
