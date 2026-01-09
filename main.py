27d4e8ece9ddd855370101087a29d3a7527d821f
OskarNerheim <157756236+OskarNerheim@users.noreply.github.com>
2026-01-09 12:39:12 +0100

    Implement find_scaled_normales method in Cell, Line, and Triangle classes; update main.py to print scaled normals

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import time
 ti_bib = time.time()
 from src.mesh import Mesh
 from src.simulation import Simulation
+import numpy as np
 
 ti_ngh = time.time()
 
 sim = Simulation(Mesh("bay.msh"))
-#sim.run_sim()
+
+sim.run_sim()
 print(sim.cells[-5].id)
 print(sim.cells[-5].ngb)
+print(sim.cells[-5].scaled_normal)
 print(sim.cells[3615].id)
 print(sim.cells[3615].ngb)
+print(sim.cells[3615].scaled_normal)
 
 tf = time.time()
 print("Time elapsed bib:", tf - ti_bib)
