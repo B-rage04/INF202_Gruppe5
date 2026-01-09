e97a8779301f8032505abaffbdf85654fa4e7a6b
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-09 10:44:52 +0100

    sdff

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from src.mesh import Mesh
 from src.simulation import Simulation
+import time
 
+ti = time.time()
 sim = Simulation(Mesh("bay.msh"))
 # sim.run_sim()
-print(sim.cells[-5].id)
-print(sim.cells[-5].ngb)
-print(sim.cells[3437].id)
-print(sim.cells[3437].id)
+print(sim.cells[3707].id)
+print(sim.cells[3707].ngb)
+print(sim.cells[3615].id)
+print(sim.cells[3615].ngb)
+tf = time.time()
+
+print(tf-ti)
