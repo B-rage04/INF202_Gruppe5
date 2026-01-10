7a00119285788931786b49706672a6832a27123f
VikingInOrbit <104018760+VikingInOrbit@users.noreply.github.com>
2026-01-10 14:39:03 +0100

    remove un needded tests

--- test/test_smoke_imports.py
+++
@@ -1,9 +0,0 @@
-from src.flow import Flow
-
-
-def test_flow_methods():  # Test just to chek the CI
-    f = Flow()
-    u = f.u0(0.35, 0.45)
-    vx, vy = f.v(1.0, 2.0)
-    assert isinstance(u, float)
-    assert isinstance(vx, float) and isinstance(vy, float)
