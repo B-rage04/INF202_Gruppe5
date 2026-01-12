a26d7798e686c8503d91fbb867d0f8ec1279f0c7
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-12 13:25:21 +0100

    Add pytest fixtures for configuration and mesh in simulation tests

--- test/UnitTest/test_simulation.py
+++ test/UnitTest/test_simulation.py
@@ -1,4 +1,21 @@
-def test_simulation():
-    from src.simulation import Simulation
+import pytest
+from src.simulation import Simulation
 
-    assert True  # TODO fix test
+@pytest.fixture
+def read_config():
+    from src.LoadTOML import LoadTOML
+    configloader = LoadTOML()
+    config = configloader.load_toml_file("Exsample\SimConfig\BaseSimConfig.toml")
+    return config
+
+def test_simulation_config(read_config):
+    """
+    Checks im the simulation collects the correct settings from the given config file.
+    Uses nSteps as an example
+    """
+    sim = Simulation(read_config)
+    assert sim.nSteps == sim.config["settings"]["nSteps"] # The simulation doesnt collect the correct settings from the config file
+
+def test_update_oil(read_config):
+    sim = Simulation(read_config)
+
