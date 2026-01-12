0b36fd2a0b02d30825c4dcd8d287ec8d360f3959
lalicon05 <lasse.l535@gmail.com>
2026-01-12 14:57:16 +0100

    almost done testing simulation

--- test/UnitTest/test_simulation.py
+++ test/UnitTest/test_simulation.py
@@ -19,8 +19,8 @@ class FakeMesh:
     def __init__(self):
         c0 = FakeCell(0, 1.0)
         c1 = FakeCell(1, 2.0)
-        c0.ngb[1]
-        c1.ngb[0]
+        c0.ngb = [1]
+        c1.ngb = [0]
         self.cells = [c0, c1]
 
 @pytest.fixture
@@ -31,9 +31,24 @@ def config():
             "IO": {"writeFrequency": 1}
         }
 
+# This only exists because some one specific person really wants to use tqdm
+class DummyTqdm:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc, tb):
+        return False
+
+    def update(self, n=1):
+        pass
+
+
 def test_sim_init(monkeypatch, config):
     from src.simulation import Simulation
-    monkeypatch.setattr("src.simulation.Mesh", lambda _: FakeMesh)
+    monkeypatch.setattr("src.simulation.Mesh", lambda _: FakeMesh())
     monkeypatch.setattr("src.simulation.Visualizer", MagicMock)
     sim = Simulation(config)
     assert  sim.dt == 1.0
@@ -59,16 +74,30 @@ def test_flux_upwind(monkeypatch, config):
     f = sim.flux(0, c0, 1)
     assert f == c0.oil * 1.0
 
-
-def test_update_oil(monkeypatch, config):
+def test_flux_downwind(monkeypatch, config):
     monkeypatch.setattr("src.simulation.Mesh", lambda _: FakeMesh())
     monkeypatch.setattr("src.simulation.Visualizer", MagicMock)
 
     sim = Simulation(config)
-    sim.update_oil()
+    c0, c1 = sim.msh.cells
 
-    for cell in sim.msh.cells:
-        assert cell.oil != 0
+    c0.scaled_normal = [np.array([-1.0, 0.0])]
+
+    f = sim.flux(0, c0, 1)
+    assert f == c1.oil * -1.0
+
+
+def test_update_oil_changes_oil(monkeypatch, config):
+    monkeypatch.setattr("src.simulation.Mesh", lambda _: FakeMesh())
+    monkeypatch.setattr("src.simulation.Visualizer", MagicMock)
+
+    sim = Simulation(config)
+    before = [cell.oil for cell in sim.msh.cells]
+
+    sim.update_oil()
+    after = [cell.oil for cell in sim.msh.cells]
+
+    assert before != after
 
 
 def test_run_sim_calls_plotting(monkeypatch, config):
@@ -77,9 +106,13 @@ def test_run_sim_calls_plotting(monkeypatch, config):
     mock_vs = MagicMock()
     monkeypatch.setattr("src.simulation.Visualizer", lambda _: mock_vs)
     monkeypatch.setattr("src.simulation.VideoCreator", MagicMock)
-    monkeypatch.setattr("src.simulation.tqdm", lambda *a, **k: a[0])
+
+    monkeypatch.setattr("src.simulation.tqdm", DummyTqdm)
+
 
     sim = Simulation(config)
     sim.run_sim(run_number=1, create_video=False)
 
     assert mock_vs.plotting.called
+
+
