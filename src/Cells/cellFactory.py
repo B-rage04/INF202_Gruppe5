352553819ec89f7569872907f4489d14ce2cd8c8
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-13 10:01:32 +0100

    pre-commit formatting

--- src/Cells/cellFactory.py
+++ src/Cells/cellFactory.py
@@ -12,16 +12,17 @@ class CellFactory:
         if key not in self.cell_types:
             self.cell_types[key] = ctype
 
-
     def __call__(self):
-  
+
         for cellblock in self.msh.cells[8:12]:
             cellType = cellblock.type
             for idx, cell in enumerate(cellblock.data):
-                self.cellList.append(self.cellTypes[cellType](self.msh,cell,idx)) 
-  
+                self.cellList.append(self.cellTypes[cellType](self.msh, cell, idx))
+
         for cell in self.cellList:
             cell.findNGB(self.cellList)
+
+        for cell in self.cellList:
             cell.findScaledNormales(self.cellList)
 
         return self.cellList
