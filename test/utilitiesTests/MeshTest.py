0bd57182670e9ed6e586e5cfa074405b18eeac38
Brage Bromset Bestvold <brage.bromset.bestvold@nmbu.no>
2026-01-15 15:18:58 +0100

    universal testing parameters

--- test/utilitiesTests/MeshTest.py
+++ test/utilitiesTests/MeshTest.py
@@ -1,10 +1,9 @@
+def meshTest():
+    from src.mesh import Mesh
+    from test.utilitiesTests.ConfigTest import configTest
+    config = configTest()
+    print(type(config))
+    msh = Mesh("test/utilitiesTests/simpleMesh.msh", config)
+    return msh
 
-
-
-from src.mesh import Mesh
-from test.utilitiesTests.config import config
-config = config()
-print(type(config))
-msh = Mesh("test/utilitiesTests/simpleMesh.msh", config)
-
-#print(msh)
+msh = meshTest()
